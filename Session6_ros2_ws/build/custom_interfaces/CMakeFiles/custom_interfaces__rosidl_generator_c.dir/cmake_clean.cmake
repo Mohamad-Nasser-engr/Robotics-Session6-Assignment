e0c49f68de75d05aa8b2953ebeaf0a1b4d165df2
Mohamad-Nasser-engr <mohamadnasser.engr@gmail.com>
2024-07-19 15:42:21 +0300

    Add custom check stock interface

---
+++ Session6_ros2_ws/build/custom_interfaces/CMakeFiles/custom_interfaces__rosidl_generator_c.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/custom_interfaces__rosidl_generator_c.dir/rosidl_generator_c/custom_interfaces/srv/detail/check_stock__functions.c.o"
+  "CMakeFiles/custom_interfaces__rosidl_generator_c.dir/rosidl_generator_c/custom_interfaces/srv/detail/check_stock__functions.c.o.d"
+  "libcustom_interfaces__rosidl_generator_c.pdb"
+  "libcustom_interfaces__rosidl_generator_c.so"
+  "rosidl_generator_c/custom_interfaces/srv/check_stock.h"
+  "rosidl_generator_c/custom_interfaces/srv/detail/check_stock__functions.c"
+  "rosidl_generator_c/custom_interfaces/srv/detail/check_stock__functions.h"
+  "rosidl_generator_c/custom_interfaces/srv/detail/check_stock__struct.h"
+  "rosidl_generator_c/custom_interfaces/srv/detail/check_stock__type_support.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/custom_interfaces__rosidl_generator_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Session6_ros2_ws/build/session6_assignment/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/mohamad/Inmind/Robotics-Session6-Assignment/Session6_ros2_ws/install/session6_assignment'
